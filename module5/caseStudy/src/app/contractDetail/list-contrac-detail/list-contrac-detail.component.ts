d97e42f6255f480b46f61807d8ceb54749ea8298
Uyen Nguyen <nguyenuyen552000@gmail.com>
2021-11-20 14:55:06 +0700

    caseStudy task 1 and 2

---
+++ module5/caseStudy/src/app/contractDetail/list-contrac-detail/list-contrac-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-contrac-detail',
+  templateUrl: './list-contrac-detail.component.html',
+  styleUrls: ['./list-contrac-detail.component.css']
+})
+export class ListContracDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
