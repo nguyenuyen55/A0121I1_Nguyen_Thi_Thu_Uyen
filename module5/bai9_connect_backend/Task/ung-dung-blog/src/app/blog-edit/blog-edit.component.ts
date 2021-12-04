812ccf26d0b79b5e3ea4cc09b5f531735f34381a
Uyen Nguyen <nguyenuyen552000@gmail.com>
2021-12-04 23:13:37 +0700

    push bai 9 backend

---
+++ module5/bai9_connect_backend/Task/ung-dung-blog/src/app/blog-edit/blog-edit.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PostService} from "../post.service";
+import {IPost} from "../post";
+
+@Component({
+  selector: 'app-blog-edit',
+  templateUrl: './blog-edit.component.html',
+  styleUrls: ['./blog-edit.component.css']
+})
+export class BlogEditComponent implements OnInit {
+  post!: any;
+  postForm!: FormGroup;
+  constructor(private route: ActivatedRoute,
+              private postService: PostService,
+              private fb: FormBuilder,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.postForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(10)]],
+      body: ['', [Validators.required, Validators.minLength(10)]]
+    });
+    // @ts-ignore
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.postService.getPostById(id).subscribe(
+      next => {
+        this.post = next;
+        this.postForm.patchValue(this.post);
+      },
+      error => {
+        console.log(error);
+        this.post = null;
+      }
+    );
+  }
+  onSubmit() {
+    if (this.postForm.valid) {
+      const { value } = this.postForm;
+      const data = {
+        ...this.post,
+        ...value
+      };
+      this.postService.updatePost(data).subscribe(
+        next => {
+          this.router.navigate(['/blog']);
+        },
+        error => console.log(error)
+      );
+    }
+  }
+}
